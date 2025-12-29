f5da7ab4d532388d20050a0339930e198d88ffb1
cmacwill1 <charles.macwilliams@gmail.com>
2025-12-28 19:11:56 -0500

    reorganizing some stuff

--- src/calculator/main.cpp
+++ src/calculator/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -12,102 +13,92 @@ public:
     Token(char k, double v):kind{k},value{v}{}
 };
 
-
-
-vector<Token> get_token_vector(){
-    vector<Token> tokens;
-    char kind = 'n';
-    double value = 0;
-    while(cin >> kind >> value) {
-        if (kind == '+' || kind == '-' || kind == '*' || kind == '/') {
-            tokens.push_back(Token(kind));
-            cout << "hello\n";
-        } else {
-            tokens.push_back(Token(kind,value));
-        }
-    }
-    return tokens;
-}
-
-Token get_token(){
-    char kind;
-    cin >> kind;
-    switch(kind) {
-        case '+': case '-': case '*': case '/': case '(': case ')': {
-            return Token(kind);
-        }
-        case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '.': {
-            cin.putback(kind);
-            double value;
-            cin >> value;
-            return Token('n',value);
-        }
-    }
-}
-
-
-string test;
-
-
-int main() {
-    //vector<Token> tokens = get_token_vector();
-    //Token t1 = get_token();
-    cout << "enter expression:\n";
-    getline(cin, test);
+vector<Token> get_token_vector(string str) {
     vector<bool> is_number;
     vector<bool> is_operator;
 
-    for (int i=0; i<test.length(); i++) {
-        if ((0x30 <= test[i] && test[i] <=0x39) || test[i] == 0x2E) {
+    for (int i=0; i<str.length(); i++) {
+        if ((0x30 <= str[i] && str[i] <=0x39) || str[i] == 0x2E) {
             is_number.push_back(1);
         } else {
             is_number.push_back(0);
         }
 
-        if (test[i] == '(' || test[i] == ')' || test[i] == '+' || test[i] == '-' || test[i] == '*' || test[i] == '/') {
+        if (str[i] == '(' || str[i] == ')' || str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/') {
             is_operator.push_back(1);
         } else {
             is_operator.push_back(0);
         }
     }
-    is_number.push_back(0); // add padding
-    is_operator.push_back(0); // add padding
+    is_number.push_back(0); // add padding to trigger getting final number
+    is_operator.push_back(0); // add padding to trigger getting final number
 
-    vector<Token> token_test;
+    vector<Token> token_str;
     int double_length = 0;
     for (int i=0; i<is_number.size(); i++) {
         if (is_number[i]) {
             ++double_length;
         } else {
             if (double_length != 0) {
-                cout << "cutting at " << i - double_length << " with snip length: " << double_length << '\n';
-                double double_val = stod(test.substr(i - double_length, double_length));
-                token_test.push_back(Token('n', double_val));
+                double double_val = stod(str.substr(i - double_length, double_length));
+                token_str.push_back(Token('#', double_val));
             }
             double_length = 0;
             if (is_operator[i]) {
-                token_test.push_back(Token(test[i]));
+                token_str.push_back(Token(str[i]));
             }
         }
     }
+    return token_str;
+}
+
+double evaluate_token_map(vector<Token> token_vector, int solve_index) {
+    int first_number = 1;
+    int second_number = 1;
+    bool is_valid = 1;
+    while(is_valid) {
+        if (token_vector[solve_index - first_number].kind != '#') {
+            first_number++;
+        } else if (token_vector[solve_index + second_number].kind != '#') {
+            second_number++;
+        } else {
+            is_valid = 0;
+        }
+    }
+
+    switch (token_vector[solve_index].kind) {
+        case '+': {
+            return token_vector[solve_index - first_number].value + token_vector[solve_index + second_number].value;
+        }
+        case '-': {
+            return token_vector[solve_index - first_number].value - token_vector[solve_index + second_number].value;
+        }
+        case '*': {
+            return token_vector[solve_index - first_number].value * token_vector[solve_index + second_number].value;
+        }
+        case '/': {
+            return token_vector[solve_index - first_number].value / token_vector[solve_index + second_number].value;
+        }
+        default: {
+            throw runtime_error("Invalid operator");
+        }
+    }
+}
 
 
-    for (Token t:token_test) {
+int main() {
+    cout << "enter expression:\n";
+    string user_input;
+    getline(cin, user_input);
+    vector<Token> token_vector = get_token_vector(user_input);
+
+
+
+    for (Token t:token_vector) {
         cout << t.kind << ' ' << t.value << '\n';
     }
     cout << '\n';
-    /*
-    char test;
-    cin >> test;
-    cin.putback(test);
-    double test2;
-    cin >> test2;
-    cout << test2 << '\n';
-    cout << "output\n";
-    for (Token x:tokens) {
-        cout << x.kind << ' ' << x.value << '\n';
-    }
 
-    cout << tokens.size() << '\n';
-    */
+    double value = evaluate_token_map(token_vector, 1);
+    cout << value << '\n';
 }
